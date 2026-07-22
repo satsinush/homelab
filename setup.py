f5dc8743f77548edeb4b3d258640bf7f9236f2ac
Andrew Needham <aneedham0577@gmail.com>
2026-07-21 19:26:32 -0500

    updated roundcube

--- setup.py
+++ setup.py
@@ -893,4 +893,8 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n\033[93mOperation cancelled by user (Ctrl+C).\033[0m")
+        sys.exit(130)
